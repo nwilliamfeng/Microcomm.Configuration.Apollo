4ec1e94f77309017592208ea8676c2f4f19b02e4
nwilliamfeng <nwilliamfeng@163.com>
2019-07-07 16:48:35 +0800

    444

---
+++ Microcomm.Configuration.Apollo.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microcomm.Configuration.Apollo.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ApolloTest().Load();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Microcomm.Configuration.Apollo/ApolloConfigurationSection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Microcomm.Configuration.Apollo
+{
+    public class ApolloConfigurationSection:ConfigurationSection
+    {
+        [ConfigurationProperty("appId",IsRequired =true)]
+         public string AppId
+        {
+            get { return base["appId"] as string; }
+            set => base["appId"] = value;
+            
+        }
+    }
+}
